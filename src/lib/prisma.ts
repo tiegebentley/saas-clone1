31d3504c396463abee5d283415df1d6f3c371374
DorianAI <dorianbuilds@gmail.com>
2025-06-02 07:50:14 +0530

    prisma sol

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,15 @@
-import { PrismaClient } from "../generated/prisma";
+// @ts-nocheck
+import { PrismaClient } from '../../prisma/generated/client';
 
-const globalForPrisma = globalThis as unknown as {
-    prisma: PrismaClient | undefined;
-};
+let prisma: PrismaClient;
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") {
-    globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV === 'production') {
+    prisma = new PrismaClient();
+} else {
+    if (!global.prisma) {
+        global.prisma = new PrismaClient();
+    }
+    prisma = global.prisma;
 }
+
+export default prisma;
